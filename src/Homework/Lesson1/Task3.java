3c0daa102b1ffcb1b7bd6103f3576d7c43dfea79
olgavoronova <ollyheen@gmail.com>
2021-05-20 23:02:51 +0300

    Lesson 1. Task 3

---
+++ src/Homework/Lesson1/Task3.java
@@ -0,0 +1,17 @@
+package Homework.Lesson1;
+
+public class Task3 {
+    public static void main(String[] args){
+        /*
+        Даны 3 целых числа. Найти количество положительных чисел в исходном наборе.
+        Подсказка: используйте Math.abs(..) для нахождения модуля числа
+        */
+
+        int a = -5;
+        int b = 10;
+        int c = 28;
+        int countPositive = ((Math.abs(a)/a + 1)/2) + ((Math.abs(b)/b + 1)/2) + ((Math.abs(c)/c + 1)/2);
+
+        System.out.println("Количество положительных чисел в исходном наборе: " + countPositive);
+    }
+}
